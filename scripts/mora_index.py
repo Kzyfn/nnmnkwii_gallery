ac1befeb3674b207e76c8c234664205b6fee67ca
Kzyfn <tomakatay@g.ecc.u-tokyo.ac.jp>
2020-05-14 14:49:54 +0900

    a

---
+++ scripts/mora_index.py
@@ -0,0 +1,23 @@
+import numpy as np
+from nnmnkwii.io import hts
+
+
+
+paths = sorted(glob(join('../data/basic5000', "label_phone_align", "*.lab")))
+
+for i, filepath in enumerate(paths):
+    label = hts.load(filepath)
+
+
+    end_times = label.end_times
+    end_index = np.array(end_times) / 50000
+
+
+    mora_index = np.array([0]*int(end_index[-1]))
+
+    mora_index = [1 if i in list(end_index.astype(int)) else 0 for i in range(end_index[-1].astype(int))]
+    indices = label.silence_frame_indices().astype(int)
+    mora_index = np.delete(mora_index, indices, axis=0)
+
+
+    np.savetxt('../data/basic5000/mora_index/mora_index_' + + str(i) + '.csv')
