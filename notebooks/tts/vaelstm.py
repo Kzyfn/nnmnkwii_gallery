2b95892c2edbaf57e7de92831980681d1615ef3e
Kzyfn <tomakatay@g.ecc.u-tokyo.ac.jp>
2020-05-19 01:06:53 +0900

    a

--- notebooks/tts/vaelstm.py
+++ notebooks/tts/vaelstm.py
@@ -302,7 +302,7 @@ optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
 # Reconstruction + KL divergence losses summed over all elements and batch
 def loss_function(recon_x, x, mu, logvar):
-    BCE = F.binary_cross_entropy(recon_x.view(-1), x.view(-1, ), reduction='sum')
+    BCE = F.mse_loss(recon_x.view(-1), x.view(-1, ), reduction='sum')#F.binary_cross_entropy(recon_x.view(-1), x.view(-1, ), reduction='sum')
 
     # see Appendix B from VAE paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
