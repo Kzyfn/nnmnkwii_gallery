e28e69dc254488053e950c6fb6a0b582155ea1cc
Kzyfn <tomakatay@g.ecc.u-tokyo.ac.jp>
2020-05-19 23:32:56 +0900

    a

--- notebooks/tts/vaelstm.py
+++ notebooks/tts/vaelstm.py
@@ -302,12 +302,14 @@ start = time.time()
 # Reconstruction + KL divergence losses summed over all elements and batch
 def loss_function(recon_x, x, mu, logvar):
     BCE = F.mse_loss(recon_x.view(-1), x.view(-1, ), reduction='sum')#F.binary_cross_entropy(recon_x.view(-1), x.view(-1, ), reduction='sum')
+    print(BCE)
 
     # see Appendix B from VAE paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
     # https://arxiv.org/abs/1312.6114
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    print(KLD)
 
     return BCE + KLD
 
