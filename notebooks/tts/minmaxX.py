520cd77b2aba6c37b67f6ea476e6b0b8ed0917ab
Kzyfn <tomakatay@g.ecc.u-tokyo.ac.jp>
2020-05-21 22:35:06 +0900

    a

---
+++ notebooks/tts/minmaxX.py
@@ -0,0 +1,156 @@
+
+
+from os.path import expanduser, join
+
+
+
+import sys
+print(sys.path)
+#sys.path.remove('/usr/local/lib/python3.7/site-packages')
+sys.path.append('/usr/local/.pyenv/versions/3.6.0/lib/python3.6/site-packages')
+sys.path.append('/Users/kazuya_yufune/.pyenv/versions/3.6.0/lib/python3.6/site-packages')
+
+import time
+
+from nnmnkwii.datasets import FileDataSource, FileSourceDataset
+from nnmnkwii.datasets import PaddedFileSourceDataset, MemoryCacheDataset#これはなに？
+from nnmnkwii.preprocessing import trim_zeros_frames, remove_zeros_frames
+from nnmnkwii.preprocessing import minmax, meanvar, minmax_scale, scale
+from nnmnkwii import paramgen
+from nnmnkwii.io import hts
+from nnmnkwii.frontend import merlin as fe
+from nnmnkwii.postfilters import merlin_post_filter
+
+from os.path import join, expanduser, basename, splitext, basename, exists
+import os
+from glob import glob
+import numpy as np
+from scipy.io import wavfile
+from sklearn.model_selection import train_test_split
+import pyworld
+import pysptk
+import librosa
+import librosa.display
+
+
+
+
+DATA_ROOT = "./data/basic5000"#NIT-ATR503/"#
+test_size = 0.01 # This means 480 utterances for training data
+random_state = 1234
+
+
+
+mgc_dim = 180#メルケプストラム次数　？？
+lf0_dim = 3#対数fo　？？ なんで次元が３？
+vuv_dim = 1#無声or 有声フラグ　？？
+bap_dim = 15#発話ごと非周期成分　？？
+
+duration_linguistic_dim = 438#question_jp.hed で、ラベルに対する言語特徴量をルールベースで記述してる
+acoustic_linguisic_dim = 442#上のやつ+frame_features とは？？
+duration_dim = 1
+acoustic_dim = mgc_dim + lf0_dim + vuv_dim + bap_dim #aoustice modelで求めたいもの
+
+fs = 48000
+frame_period = 5
+fftlen = pyworld.get_cheaptrick_fft_size(fs)
+alpha = pysptk.util.mcepalpha(fs)
+hop_length = int(0.001 * frame_period * fs)
+
+mgc_start_idx = 0
+lf0_start_idx = 180
+vuv_start_idx = 183
+bap_start_idx = 184
+
+windows = [
+    (0, 0, np.array([1.0])),
+    (1, 1, np.array([-0.5, 0.0, 0.5])),
+    (1, 1, np.array([1.0, -2.0, 1.0])),
+]
+
+use_phone_alignment = True
+acoustic_subphone_features = "coarse_coding" if use_phone_alignment else "full" #とは？
+
+
+
+
+class BinaryFileSource(FileDataSource):
+    def __init__(self, data_root, dim, train):
+        self.data_root = data_root
+        self.dim = dim
+        self.train = train
+    def collect_files(self):
+        files = sorted(glob(join(self.data_root, "*.bin")))
+        files = files[:len(files)-5] # last 5 is real testset
+
+        train_files, test_files = train_test_split(files, test_size=test_size,
+                                                   random_state=random_state)
+        if self.train:
+            return train_files
+        else:
+            return test_files
+    def collect_features(self, path):
+        return np.fromfile(path, dtype=np.float32).reshape(-1, self.dim)
+
+
+X = {"acoustic": {}}
+Y = {"acoustic": {}}
+utt_lengths = { "acoustic": {}}
+for ty in ["acoustic"]:
+    for phase in ["train", "test"]:
+        train = phase == "train"
+        x_dim = duration_linguistic_dim if ty == "duration" else acoustic_linguisic_dim
+        y_dim = duration_dim if ty == "duration" else acoustic_dim
+        X[ty][phase] = FileSourceDataset(BinaryFileSource(join(DATA_ROOT, "X_{}".format(ty)),
+                                                       dim=x_dim,
+                                                       train=train))
+        Y[ty][phase] = FileSourceDataset(BinaryFileSource(join(DATA_ROOT, "Y_{}".format(ty)),
+                                                       dim=y_dim,
+                                                       train=train))
+        utt_lengths[ty][phase] = np.array([len(x) for x in X[ty][phase]], dtype=np.int)
+
+
+
+
+
+for ty in ["acoustic"]:
+    for phase in ["train", "test"]:
+        train = phase == "train"
+        x_dim = duration_linguistic_dim if ty == "duration" else acoustic_linguisic_dim
+        y_dim = duration_dim if ty == "duration" else acoustic_dim
+        X[ty][phase] = FileSourceDataset(BinaryFileSource(join(DATA_ROOT, "X_{}".format(ty)),
+                                                       dim=x_dim,
+                                                       train=train), 
+                                               )#np.max(utt_lengths[ty][phase]))
+        Y[ty][phase] = FileSourceDataset(BinaryFileSource(join(DATA_ROOT, "Y_{}".format(ty)),
+                                                       dim=y_dim,
+                                                       train=train), 
+                                              )# np.max(utt_lengths[ty][phase]))
+
+
+
+
+
+print("Total number of utterances:", len(utt_lengths["acoustic"]["train"]))
+print("Total number of frames:", np.sum(utt_lengths["acoustic"]["train"]))
+
+
+
+
+X_min = {}
+X_max = {}
+Y_mean = {}
+Y_var = {}
+Y_scale = {}
+
+for typ in ["acoustic"]:
+    X_min[typ], X_max[typ] = minmax(X[typ]["train"], utt_lengths[typ]["train"])
+    Y_mean[typ], Y_var[typ] = meanvar(Y[typ]["train"], utt_lengths[typ]["train"])
+    Y_scale[typ] = np.sqrt(Y_var[typ])
+
+np.savetxt('x_min.csv', X_min['acoustic'])
+np.savetxt('x_max.csv', X_max['acoustic'])
+np.savetxt('y_mean.csv', Y_mean['acoustic'])
+np.savetxt('y_var.csv', Y_var['acoustic'])
+
+
