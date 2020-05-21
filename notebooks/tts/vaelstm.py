d970e62101a4aa609f812826f0829285ee45070d
Kzyfn <tomakatay@g.ecc.u-tokyo.ac.jp>
2020-05-21 15:27:40 +0900

    z修正後0.01kld

--- notebooks/tts/vaelstm.py
+++ notebooks/tts/vaelstm.py
@@ -447,7 +447,7 @@ for epoch in range(1, num_epochs + 1):
 # save the training model
 np.save('loss_list.npy', np.array(loss_list))
 np.save('test_loss_list.npy', np.array(test_loss_list))
-torch.save(model.state_dict(), 'vae_mse_0.1kld_z_changed.pth')
+torch.save(model.state_dict(), 'vae_mse_0.01kld_z_changed.pth')
 
 
 # ## Train
