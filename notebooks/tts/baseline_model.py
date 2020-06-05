fb24b3e5ae3fec01eda815d63fe3a8b2f654b6de
Kzyfn <tomakatay@g.ecc.u-tokyo.ac.jp>
2020-06-05 10:15:12 +0900

    a

--- notebooks/tts/baseline_model.py
+++ notebooks/tts/baseline_model.py
@@ -176,19 +176,13 @@ optimizer = optim.Adam(model.parameters(), lr=2e-3)#1e-3
 start = time.time()
 
 # Reconstruction + KL divergence losses summed over all elements and batch
-def loss_function(recon_x, x, mu, logvar):
+def loss_function(recon_x, x):
     MSE = F.mse_loss(recon_x.view(-1), x.view(-1, ), reduction='sum')#F.binary_cross_entropy(recon_x.view(-1), x.view(-1, ), reduction='sum')
     #print('LOSS')
     #print(BCE)
 
-    # see Appendix B from VAE paper:
-    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-    # https://arxiv.org/abs/1312.6114
-    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-    #print(KLD)
 
-    return MSE +  0.1 * KLD
+    return MSE
 
 
 
