15f530d9a2eed1e77e57bf0cd7328d86515b8458
Kzyfn <tomakatay@g.ecc.u-tokyo.ac.jp>
2020-06-12 14:27:55 +0900

    a

--- scripts/mora_index.py
+++ scripts/mora_index.py
@@ -28,5 +28,6 @@ for i, filepath in tqdm(enumerate(paths)):
     mora_index = [1 if i in list(end_index.astype(int)) else 0 for i in range(end_index[-1].astype(int)+1)]
     indices = label.silence_frame_indices().astype(int)
     mora_index = np.delete(mora_index, indices, axis=0)
+    mora_index = mora_index.nonzero()
 
-    np.savetxt('../data/basic5000/mora_index/mora_index_' + '0'*(4-len(str(i+1))) + str(i+1) + '.csv', mora_index)
+    np.savetxt('../data/basic5000/mora_index/squeezed_mora_index_' + '0'*(4-len(str(i+1))) + str(i+1) + '.csv', mora_index)
